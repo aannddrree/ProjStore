c92806e6b9b98c2eed89337405b3ed526441fd9b
= <andre.silva@hubdoctor.com.br>
2023-04-18 11:58:56 -0300

    Adicionar arquivos de projeto.

---
+++ ProjStore/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Controllers;
+using Models;
+
+Console.WriteLine("Projeto Loja");
+
+var data = new Store()
+{
+    Description = "Teste",
+    Address = "Rua do alem"
+};
+
+string returnInformation = (new StoreController().Insert(data) ? "Inserido" : "Erro");
+Console.WriteLine(returnInformation);
+
+new StoreController().GetAll().ForEach(x => Console.WriteLine(x));
